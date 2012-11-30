c107000cc82508e7ff7da97232b3658275d220d1
Larry Buchanan <larrybuch@gmail.com>
2012-11-30 15:43:49 -0500

    inital commit, client server

---
+++ communikator-client/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommunikatorClient::Application.config.secret_token = '6837edaa466eb8c753d5948ab3e00ddd5a6a29dd3560a442210e7bd681550cf2566ac39cc1593884f464096eb70da2e6e0b350ecb9ca9d54e481193892c9cb59'
---
+++ communikator-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommunikatorServer::Application.config.secret_token = '843e16143bf58a5055ac22849fb89e0f1d60cf3dfff83e5931ac9b3d7765d1df178c8e4a9efc10a09aeaf69fd1c4693c58edeff12e11452c55f30dcd5966a813'
